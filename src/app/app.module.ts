7df7f171ce30fa0f38949c157c5619f739c752ea
saishree <Saishree@leadwinner.com>
2022-08-17 19:04:14 +0530

    added components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { HeaderComponent } from './header/header/header.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {ShoppingComponent} from './shopping/shopping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
