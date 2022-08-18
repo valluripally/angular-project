aba016c4b825d81d50cb872ae757427b1d87bc06
saishree <Saishree@leadwinner.com>
2022-08-18 17:56:09 +0530

    added shooping list , recipe and models

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+ 
+  recipes:Recipe[]=[new Recipe('test-recipe','test',"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg")];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
     selector:'app-shopping',
@@ -6,5 +7,7 @@ import { Component } from "@angular/core";
     styleUrls:['./shopping.component.scss']
 })
 export class ShoppingComponent{
+
+    ingredients:Ingredient[]=[new Ingredient("apples",5), new Ingredient("tomatos",10)];
 
 }
