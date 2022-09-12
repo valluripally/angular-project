d90ec130e3b56ef576c8dd0349486e8e82ff0901
saishree <Saishree@leadwinner.com>
2022-09-12 19:01:18 +0530

    component intercation in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  
+  recipe =false;
+  shopping=false;
+listenToHeader(name:String){
+
+  if(name ==="Recipe")
+ {
+  this.recipe =true;
+  this.shopping =false;
+ } 
+   if(name ==="Shopping")
+  {
+    this.shopping =true;
+    this.recipe =false;
+  }
+ 
+}
+
 }
--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+  @Output() selectedTab=new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  selectedValue(name:string ){
+    this.selectedTab.emit(name);
+  }
+
+
 
 }
